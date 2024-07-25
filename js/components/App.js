25fd95aa86d675206baf1321495e5446e33b1b52
Carlos Pinho <72576083+carlospinhod@users.noreply.github.com>
2024-07-26 00:25:56 +0100

    EP 7
    Bring it All Together

--- js/components/App.js
+++ js/components/App.js
@@ -1,6 +1,8 @@
-import AppButton from "./AppButton.js";
-export default{
-    components: {
-        'app-button' : AppButton
-    }
+import Assignments from "./Assignments.js";
+
+export default {
+    components: { Assignments },
+    template: `
+            <assignments></assignments>
+    `
 }
