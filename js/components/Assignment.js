ec0d3800607c63e637ca402bf07b23c4d1c82978
Carlos Pinho <72576083+carlospinhod@users.noreply.github.com>
2024-07-30 21:19:57 +0100

    EP 8
    Handle a Form Submission

--- js/components/Assignment.js
+++ js/components/Assignment.js
@@ -1,11 +1,11 @@
 export default {
     template: `
-        <li>
-            <label>
-                {{ assignment.name }}
-                <input type="checkbox" v-model="assignment.complete">
-            </label>
-        </li>
+      <li>
+        <label class="p-2 flex justify-between align-center">
+          {{ assignment.name }}
+          <input type="checkbox" v-model="assignment.complete" class="ml-3">
+        </label>
+      </li>
     `,
     props: {
         assignment: Object
