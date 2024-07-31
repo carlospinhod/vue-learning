51ab2489b68745cdca0730f18655b9a6083cb050
Carlos Pinho <72576083+carlospinhod@users.noreply.github.com>
2024-07-31 19:15:25 +0100

    EP 11
    Component Responsibility

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,24 +1,19 @@
 import Assignment from "./Assignment.js";
+import AssignmentTags from "./AssignmentTags.js";
 
 export default {
-    components: {Assignment},
+    components: {AssignmentTags, Assignment},
 
     template: `
       <section v-show="assignments.length">
 
         <h2 class="font-bold mb-2">{{ title }} <span> ({{ assignments.length }})</span></h2>
 
-        <div class="flex gap-2 ">
-          <button
-              @click="currentTag = tag"
-              v-for="tag in tags"
-              class="border rounded px-1 py-px text-sm "
-              :class="{
-                  'border-blue-500 text-blue-500': tag === currentTag,
-              }"
-          >{{ tag }}
-          </button>
-        </div>
+        <assignment-tags
+            :initial-tags="assignments.map(a => a.tag)"
+            :current-tag="currentTag"
+            @change="currentTag = $event"
+        />
 
         <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
           <assignment
@@ -47,9 +42,6 @@ export default {
             return this.assignments.filter(a => a.tag === this.currentTag);
 
 
-        },
-        tags() {
-            return ['all', ...new Set(this.assignments.map(a => a.tag))];
         }
     }
 }
